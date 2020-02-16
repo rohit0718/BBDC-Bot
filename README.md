4d682a026566929bdb4c504334216a509bc10b94
Rohit <rohit.rajesh@versafleet.co>
2020-02-16 19:42:42 +0800

    Initial commit including version 1 of bbdc bot and updated README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# BBDC-Bot
-BBDC slot bot built with Selenium and Python
+# BBDC Bot
+
+Simple bot built with Selenium using `python` as the language which automatically select and confirms your favoured slots automatically with just one command `python bbdc_bot.py`. Built for those who are looking for last minute slots and are tired of having to always enter all their credentials, manually select favoured slots, and click past countless dialogues, only to be presented with `No more slots available.`
+
+
+## Prerequisties
+
+- selenium & seleneium chrome driver (or any other browser of your choice)
+- chrome
+- python
+- basic python knowledge (at least how to access elements in a list)
+
+
+## Config
+
+Open the `bbdc_bot.py` file with your favourite IDE and change these following lines of code:
+- `chromedriver_location` in line 5 with the path of your `chromedriver` that you had downloaded
+- `username` and `password` in lines 7 and 8
+- required `months` (line 15),`sessions` (line 19) `days` (line 23), and `slots` (line 25). These are all lists that contain all of the various options that you see in the UI, with elements in chronological order (first month is `months[0]`, second is `months[1]`, etc.)
+
+
+## Steps to run
+
+1) Navigate to folder with `bbdc_bot.py`
+2) Run `python bbdc_bot.py`
+3) Sit back and watch the bot select and confirm your selection automatically :)
+
+
+If you have any enquries feel free to contact me and, of course, feel free to use this for your very own versions of the bot (including a fully functional UI, infintitely looping program that only quits once its found a slot, for e.g.).
---
+++ bbdc_bot.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+browser = webdriver.Chrome('insert_path_to_chromedriver_here')
+
+username = 'insert_your_username_here'
+password = 'insert_your_password_here'
+
+browser.get('https://info.bbdc.sg/members-login/')
+idLogin = browser.find_element_by_id('txtNRIC')
+idLogin.send_keys(username)
+idLogin = browser.find_element_by_id('txtPassword')
+idLogin.send_keys(password)
+loginButton = browser.find_element_by_name('btnLogin')
+loginButton.click()
+
+
+# Switching to Left Frame and accessing element by text
+browser.switch_to.default_content()
+frame = browser.find_element_by_name('leftFrame')
+browser.switch_to.frame(frame)
+nonFixedInstructor = browser.find_element_by_link_text('Booking without Fixed Instructor')
+nonFixedInstructor.click()
+
+# Switching back to Main Frame and pressing 'I Accept'
+browser.switch_to.default_content()
+wait = WebDriverWait(browser, 300)
+wait.until(EC.frame_to_be_available_and_switch_to_it(browser.find_element_by_name('mainFrame')))
+wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "btn"))).click()
+
+# Selection menu
+browser.switch_to.default_content()
+wait = WebDriverWait(browser, 300)
+wait.until(EC.frame_to_be_available_and_switch_to_it(browser.find_element_by_name('mainFrame')))
+wait.until(EC.visibility_of_element_located((By.ID, "checkMonth")))
+
+# 0 refers to first month, 1 refers to second month, and so on...
+months = browser.find_elements_by_id('checkMonth')
+months[12].click() # all months
+
+# 0 refers to first session, 1 refers to second session, and so on...
+sessions = browser.find_elements_by_id('checkSes')
+sessions[8].click() # all sessions
+
+# 0 refers to first day, 1 refers to second day, and so on...
+days = browser.find_elements_by_id('checkDay')
+days[7].click() # all days
+
+# Selecting Search
+browser.find_element_by_name('btnSearch').click()
+
+# Dismissing Prompt
+wait = WebDriverWait(browser, 300)
+wait.until(EC.alert_is_present())
+alert_obj = browser.switch_to.alert
+alert_obj.accept()
+wait.until(EC.visibility_of_element_located((By.NAME, "slot")))
+
+# 0 refers to first slot, 1 refers to second slot, and so on...
+slots = browser.find_elements_by_name('slot')
+for slot in slots:     # Selecting all checkboxes
+    slot.click()
+    browser.find_element_by_class_name('pgtitle').click()     # clicking random element to hide hover effect
+
+# Selecting Submit
+browser.find_element_by_name('btnSubmit').click()
+
+# Selecting confirm
+wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@value='Confirm']")))
+browser.find_element_by_xpath("//input[@value='Confirm']").click()
+
